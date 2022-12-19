ed8c796935b20442556fbd42635e5269a1a71395
Areeb <areeb.alam@oncehub.com>
2022-12-19 12:19:24 +0530

    updated app for local running

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.1.110:9090'
+    apiUrl: 'http://localhost:9090'
   };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.1.110:9090'
+    apiUrl: 'http://localhost:9090'
   };
